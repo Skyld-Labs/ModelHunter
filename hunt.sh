8420e6192bf3b698562fe57dc708c321425461bc
Skyld <marie@skyld.io>
2023-06-13 11:22:17 +0200

    Update hunt.sh

--- hunt.sh
+++ hunt.sh
@@ -47,9 +47,9 @@ while getopts ":vshp:d:" option; do
 	  	echo "Usage $0 [-v][-s][-h][-p {package_id}][-d]{directory with apk}"
 		echo """
 		-v | Verbose
-		-s | Scrap package id on Google play, keyword for research are inside search-keyword.txt
-		-p {package_id}| APK to test, it will be download, then test result can be found inside APP_RESULT, and databse
-		-d {/path/to/apk_directory} | Directory with APK to test 
+		-s | Scrap package id on Google play, keywords for research are inside search-keyword.txt
+		-p {package_id}| APK to test, it will be downloaded, the results can be found inside APP_RESULT and the database
+		-d {/path/to/apk_directory} | Directory with APKs to test 
 		"""
 		exit 0
 		;;
@@ -63,7 +63,7 @@ done
 # param : 
 # app_name download_directory
 static_analysis(){
-	echo "Start Static analysis"
+	echo "Starting Static analysis"
 	save_path="$PWD"
 	dir_test="$APP_RESULT/$1"
 	create_dir_ifnexist $dir_test
@@ -73,7 +73,7 @@ static_analysis(){
 	
 	cd $save_path
 	cp "$MODELHUNTER_PATH/output_dir/$1/report.md" $dir_test
-	# If model is found 
+	# If a model is found 
 	if [[ $(ls "$MODELHUNTER_PATH/output_dir/models/" | wc -c) -gt 0 ]]; then
 		create_dir_ifnexist "$dir_test/models"
 		for i in $(ls "$MODELHUNTER_PATH/output_dir/models/")
@@ -93,7 +93,7 @@ scrapping(){
 	for i in $(cat $KEYWORD_SCRAPPING)
 	do	
 		if [[ $SCRAPING == 'TRUE' ]]; then
-			 echo "Scrap for $i"
+			 echo "Scrapping keyword $i"
 			./scrapping-playstore.sh -rq $i >> $APP_ID
 		fi
 	done
@@ -108,39 +108,39 @@ analyse(){
 	DEST_APK=$(readlink -e $2)
 	# Download APK with selenium on firefox
 	if [[ ! -f "$DEST_APK/$APP_ID.apk" ]] ; then 
-		echo "Try download it $APP_ID" 
+		echo "Trying to download $APP_ID" 
 		$DOWNLOADER $APP_ID $DEST_APK/
 	else 
-		echo "Already download $APP_ID" 
+		echo "Already downloaded $APP_ID" 
 	fi
 
 	if [[ $? != -1 ]] ; then
-		# Give app name and it will go search in the right directory 
+		# Give an app name and it will go search in the right directory 
 		static_analysis "$APP_ID" "$DEST_APK"
 		if [[ $SAVE_APK == 'FALSE' ]]; then
-			echo "File supress"
+			echo "File supressed"
 			rm $APP_ID/$APP_ID.apk
 		fi
 	fi
 }
 #################################################################################
-### 	Analyse list of App			
+### 	Analyse list of Apps			
 #################################################################################
 
-# Take in argument a list of app
+# Take as argument a list of apps
 analyse_list(){
 	LIST=$@
 	MAX=$#
 	COUNT=0
 	for app in $LIST; do
 		echo "###"
-		echo "# Test $COUNT/$MAX with application : $app"
+		echo "# Test $COUNT/$MAX application : $app"
 		echo "###"
 
 		COUNT=$(($COUNT+1))
 		DIFF=$(awk '$0 == "'$app'"' $APP_TESTED | wc -l) 
 		
-		# Search if application is already tested, if not test it
+		# Search if the application is already tested, if not test it
 		if [[ $DIFF -eq 0 ]] ; then
 			# Analyse app 
 			analyse "$app" $APK_DIRECTORY
@@ -161,20 +161,20 @@ if [[ $VERBOSE == 'TRUE' ]]; then
 fi
 
 if [[ "$PACKAGE_ID" != "" ]]; then 
-	echo "Start analysis of package $PACKAGE_ID"
+	echo "Starting analysis of package $PACKAGE_ID"
 	analyse $PACKAGE_ID $APK_DIRECTORY
 	exit 1
 fi
 
 if [[ $SCRAPING == 'TRUE' ]]; then 
 	# analyse application find by scrapping Google Play
-	echo "Start scrapping, list of application will be write in $APP_ID"
+	echo "Starting scrapping, list of application will be writen in $APP_ID"
 	scrapping
 	analyse_list $(cat $APP_ID)
 else
 	# analyse application download inside APK_DIRECTORY
 	echo "Use 'config.sh' as base configuration."
 	echo "..."
-	echo "Start analyse application inside $APK_DIRECTORY"
+	echo "Starting analyse of applications inside $APK_DIRECTORY"
 	analyse_list $(ls $APK_DIRECTORY | sed 's/.apk//g')	
 fi
