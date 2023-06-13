0c5d6597823a30e63147e79c70832f28a0ec0da5
Skyld <marie@skyld.io>
2023-06-13 11:28:23 +0200

    Update sort-report.sh

--- sort-report.sh
+++ sort-report.sh
@@ -4,7 +4,7 @@ APP_RESULT='APP_RESULT/'
 SORT_RESULT='SORT_RESULT'
 
 #################################################################################
-###				Test File Exist
+###				Test File Exists
 #################################################################################
 
 create_dir_ifnexist(){
@@ -25,46 +25,46 @@ create_dir_ifnexist $SORT_RESULT
 
 cd $APP_RESULT
 
-echo -n "Number of applications try to test (some error is not check for the moment) : "
+echo -n "Number of applications tested (some errors were not checked for the moment) : "
 cat ../app_ids_tested.txt | wc -l
 
-echo -n "Number of Report :"
+echo -n "Number of Reports :"
 find . -name report.md | wc -l
 
-echo -n "Number of application with AI keyword found :"
+echo -n "Number of applications with AI keyword found :"
 find . -name report.md -exec bash -c "grep -q 'magic word .*:' {} && echo {}" \; | grep -o "./[^/]*/" | sed "s/\(\.\/\|\/\)//g"  > ../$SORT_RESULT/'app_with_keyword_found.txt'
 find . -name report.md -exec bash -c "grep -q 'magic word .*:' {} && echo {}" \; | grep -o "./[^/]*/" | wc -l
 
-echo -n "Number of application with AI keyword found (without cnn, rnn) :"
+echo -n "Number of applications with AI keyword found (without cnn, rnn) :"
 find . -name report.md -exec bash -c "cat {} | grep 'magic word' | grep -v 'cnn\|rnn' && echo {}" \; | grep -o "./[^/]*/" | sed "s/\(\.\/\|\/\)//g" > ../$SORT_RESULT/'app_with_keyword_found_without_cnn_rnn.txt'
 find . -name report.md -exec bash -c "cat {} | grep 'magic word' | grep -v 'cnn\|rnn' && echo {}" \; | grep -o "./[^/]*/" | wc -l
 
-echo -n "Number of model found (can contais false positive) : "
+echo -n "Number of models found (can contain false positives) : "
 find . -type f -wholename "*/models/*" -exec echo {} \; | wc -l
 
-echo -n "Number of model encrypted found (can contais false positive) : "
+echo -n "Number of models encrypted found (can contain false positives) : "
 for i in $(find . -type f -wholename "*/models/*" -exec echo {} \; | sort | cut -d'/' -f2 | uniq); do  cat $i/report.md | grep '^[0-9]\.[0-9]*'; done | grep '^[7-9]\.9[0-9]*' | wc -l
 
-echo -n "Number of application that contains models :"
+echo -n "Number of applications that contain models :"
 find . -name models -exec echo {} \; | grep -o "./[^/]*/" | sed "s/\(\.\/\|\/\)//g" > ../$SORT_RESULT/'app_with_models.txt'
 find . -name models -exec echo {} \; | grep -o "./[^/]*/" | wc -l
 
-echo -n "Number of application which have models and keyword : "
+echo -n "Number of applications which have models and keywords : "
 comm -12 <(find . -name models -exec echo {} \; | grep -o "./[^/]*/" | sort) <(find . -name report.md -exec bash -c "grep -q 'magic word .*:' {} && echo {}" \; | grep -o "./[^/]*/" | sort ) | sed "s/\(\.\/\|\/\)//g" > ../$SORT_RESULT/'app_with_models_and_keyword.txt'
 comm -12 <(find . -name models -exec echo {} \; | grep -o "./[^/]*/" | sort) <(find . -name report.md -exec bash -c "grep -q 'magic word .*:' {} && echo {}" \; | grep -o "./[^/]*/" | sort ) | wc -l
 
-echo -n "Number of application which haven't models and keyword tensorflow : "
+echo -n "Number of applications which haven't models and keyword tensorflow : "
 comm -12 <(find . -type d -exec test -e "{}/models" ';' -prune -o -print |  grep -o "./[^/]*/" | sort -u) <(find . -name report.md -exec bash -c "grep -q 'magic word tensorflow:' {} && echo {}" \; | grep -o "./[^/]*/" | sort ) | sed "s/\(\.\/\|\/\)//g" > ../$SORT_RESULT/'app_without_models_and_keyword_tensorflow.txt'
 comm -12 <(find . -type d -exec test -e "{}/models" ';' -prune -o -print |  grep -o "./[^/]*/" | sort -u) <(find . -name report.md -exec bash -c "grep -q 'magic word tensorflow:' {} && echo {}" \; | grep -o "./[^/]*/" | sort ) | wc -l
 
-echo -n "Number of application with a TensorFlow models : "
+echo -n "Number of applications with a TensorFlow models : "
 find . -type f -wholename "*/models/*" -exec echo {} \; | grep '\.tfl' | sort | cut -d'/' -f2 | uniq > ../$SORT_RESULT/'app_with_tensorflow_model.txt'
 find . -type f -wholename "*/models/*" -exec echo {} \; | grep '\.tfl' | sort | cut -d'/' -f2 | uniq | wc -l
 
 echo -n "Number of Tensorflow models : "
 find . -type f -wholename "*/models/*" -exec echo {} \; | grep '\.tfl' | sort | cut -d'/' -f4 | uniq | wc -l
 
-echo -n "Number of application contains Tensorflow encrypted models : "
+echo -n "Number of applications containing Tensorflow encrypted models : "
 find . -name report.md -exec bash -c "cat {} | grep '\.tfl' | grep '^[7-9]\.9[0-9]*'" \; -exec echo {} \; | grep -o "./[^/]*/" | sed "s/\(\.\/\|\/\)//g" > ../$SORT_RESULT/'app_with_encrypted_models.txt'
 find . -name report.md -exec bash -c "cat {} | grep '\.tfl' | grep '^[7-9]\.9[0-9]*'" \; -exec echo {} \; | grep -o "./[^/]*/" | sed "s/\(\.\/\|\/\)//g" | wc -l
 
